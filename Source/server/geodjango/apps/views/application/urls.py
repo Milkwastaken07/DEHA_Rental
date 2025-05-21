8c4033ab56057663d0e4c8969232f71928629616
Thái Sơn <sonluc121@gmail.com>
2025-05-22 02:01:43 +0700

    new4

--- Source/server/geodjango/apps/views/application/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('/', createApplication, name='createApplication'),
-    path('/:id/status', updateApplicationStatus, name='updateApplicationStatus'),
-    path('/', listApplications, name='listApplications'),
-]
--- Source/server/geodjango/apps/views/lease/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import getLeases, getLeasePayments
-urlpatterns = [
-    path('/', getLeases, name='getLeases'),
-    path('/:id/payments', getLeasePayments, name='getLeasePayments'),
-]
